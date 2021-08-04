c963cdbd7a0c9f269cc7622a89c6b887a2f82b7d
S-aLeMDeV <88463985+S-aLeMDeV@users.noreply.github.com>
2021-08-05 01:12:37 +0200

    vers1

---
+++ s.cpp
@@ -0,0 +1,225 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main(){
+
+// School Subjets ::
+
+int sc,ma,ar,en,fr,ss,pc,rn;
+
+string li = getenv("LOGNAME");
+system("clear");
+cout << "\n\n\n=============================================================\n";
+
+cout << "\n  !";
+sleep(1);
+cout <<"!";
+sleep(1);
+cout<<" W";
+sleep(1);
+cout<<"e";
+sleep(1);
+cout<<"l";
+sleep(1);
+cout<<"c";
+sleep(1);
+cout<<"o";
+sleep(1);
+cout<<"m";
+sleep(1);
+cout<<"e ";
+sleep(1);
+cout<<li<<" :).\n\n";
+sleep(1);
+
+cout << "=============================================================\n";
+
+cout << "  [+] Please Note That Each Subject Marks Equal 40 At Maxim !!\n";
+
+cout << "=============================================================\n";
+
+
+cout <<"\n\n\n\n";
+
+cout<<"  [-] Give Me Ur Maths Marks , Ma friend : ";
+cin>>ma;
+cout<<"\n";
+cout<<"  [-] Give Me Ur Sciences Marks : ";
+cin>>sc;
+cout<<"\n";
+cout<<"  [-] Give Me Ur English M : ";
+cin>>en;
+cout<<"\n";
+cout<<"  [-] Ur Arabic M : ";
+cin>>ar;
+cout<<"\n";
+cout<<"  [-] Ur Frinsh M : ";
+cin>>fr;
+cout<<"\n";
+cout<<"  [-] Ur Computer M : ";
+cin>>pc;
+cout<<"\n";
+cout<<"  [-] Ur Social Studi. M : ";
+cin>>ss;
+cout<<"\n";
+cout<<"  [-] Ur Religion M : ";
+cin>>rn;
+cout<<"\n";
+cout << "=============================================================\n";
+cout << "  [+] Please w8 :) ...\n\n\n";
+
+
+sleep(1);
+if (ma > 40){
+    cout << " Error :( !!\n";
+}else if(ma >= 30 ){
+    cout << "  [+] Exclant In Maths, My SoSo:)\n";
+}else if(ma >= 20 ){
+    cout << "  [+] GooD In Maths, My SoSo:)\n";
+}else if(ma >= 10 ){
+    cout << "  [+] U Will Maybe Pass In Maths, My SoSo:)\n";
+}else if(ma >= 0 ){
+    cout << "  [+] U Have A Good Future In Kitchen With Ur Mark In Maths, My SoSo:)\n";
+}else{
+    cout <<"  SomeThing Went Wrong :( ,\n";
+}
+
+
+cout <<"-\n";
+sleep(1);
+if (sc > 40){
+    cout << " Error :( !!\n";
+}else if(sc >= 30 ){
+    cout << "  [+] Exclant In Sciences, My SoSo:)\n";
+}else if(sc >= 20 ){
+    cout << "  [+] GooD In Sciences, My SoSo:)\n";
+}else if(sc >= 10 ){
+    cout << "  [+] U Will Maybe Pass In Sciences, My SoSo:)\n";
+}else if(sc >= 0 ){
+    cout << "  [+] U Have A Good Future In Kitchen With Ur Mark In Sciences, My SoSo:)\n";
+}else{
+    cout <<"  SomeThing Went Wrong :( ,\n";
+}
+
+
+cout <<"-\n";
+sleep(1);
+if (en > 40){
+    cout << " Error :( !!\n";
+}else if(en >= 30 ){
+    cout << "  [+] Exclant In English, My SoSo:)\n";
+}else if(en >= 20 ){
+    cout << "  [+] GooD In English, My SoSo:)\n";
+}else if(en >= 10 ){
+    cout << "  [+] U Will Maybe Pass In English, My SoSo:)\n";
+}else if(en >= 0 ){
+    cout << "  [+] U Have A Good Future In Kitchen With Ur Mark In English, My SoSo:)\n";
+}else{
+    cout <<"  SomeThing Went Wrong :( ,\n";
+}
+
+
+cout <<"-\n";
+sleep(1);
+if (en > 40){
+    cout << " Error :( !!\n";
+}else if(en >= 30 ){
+    cout << "  [+] Exclant In Arabic, My SoSo:)\n";
+}else if(en >= 20 ){
+    cout << "  [+] GooD In Arabic, My SoSo:)\n";
+}else if(en >= 10 ){
+    cout << "  [+] U Will Maybe Pass In Arabic, My SoSo:)\n";
+}else if(en >= 0 ){
+    cout << "  [+] U Have A Good Future In Kitchen With Ur Mark In Arabic, My SoSo:)\n";
+}else{
+    cout <<"  SomeThing Went Wrong :( ,\n";
+}
+
+
+cout <<"-\n";
+sleep(1);
+if (fr > 40){
+    cout << " Error :( !!\n";
+}else if(fr >= 30 ){
+    cout << "  [+] Exclant In Frinsh, My SoSo:)\n";
+}else if(fr >= 20 ){
+    cout << "  [+] GooD In Frinsh, My SoSo:)\n";
+}else if(fr >= 10 ){
+    cout << "  [+] U Will Maybe Pass In Frinsh, My SoSo:)\n";
+}else if(fr >= 0 ){
+    cout << "  [+] U Have A Good Future In Kitchen With Ur Mark In Frinsh, My SoSo:)\n";
+}else{
+    cout <<"  SomeThing Went Wrong :( ,\n";
+}
+
+
+cout <<"-\n";
+sleep(1);
+if (pc > 40){
+    cout << " Error :( !!\n";
+}else if(pc >= 30 ){
+    cout << "  [+] Exclant In Computer, My SoSo:)\n";
+}else if(pc >= 20 ){
+    cout << "  [+] GooD In Computer, My SoSo:)\n";
+}else if(pc >= 10 ){
+    cout << "  [+] U Will Maybe Pass In Computer, My SoSo:)\n";
+}else if(pc >= 0 ){
+    cout << "  [+] U Have A Good Future In Kitchen With Ur Mark In Computer, My SoSo:)\n";
+}else{
+    cout <<"  SomeThing Went Wrong :( ,\n";
+}
+
+
+cout <<"-\n";
+sleep(1);
+if (ss > 40){
+    cout << " Error :( !!\n";
+}else if(ss >= 30 ){
+    cout << "  [+] Exclant In Social Studi, My SoSo:)\n";
+}else if(ss >= 20 ){
+    cout << "  [+] GooD In Social Studi, My SoSo:)\n";
+}else if(ss >= 10 ){
+    cout << "  [+] U Will Maybe Pass In Social Studi, My SoSo:)\n";
+}else if(ss >= 0 ){
+    cout << "  [+] U Have A Good Future In Kitchen With Ur Mark In Social Studi, My SoSo:)\n";
+}else{
+    cout <<"  SomeThing Went Wrong :( ,\n";
+}
+
+
+cout <<"-\n";
+sleep(1);
+if (rn > 40){
+    cout << " Error :( !!\n";
+}else if(rn >= 30 ){
+    cout << "  [+] Exclant In Religion, My SoSo:)\n";
+}else if(rn >= 20 ){
+    cout << "  [+] GooD In Religion, My SoSo:)\n";
+}else if(rn >= 10 ){
+    cout << "  [+] U Will Maybe Pass In Religion, My SoSo:)\n";
+}else if(rn >= 0 ){
+    cout << "  [+] U are kafir, Brah :)\n";
+}else{
+    cout <<"  SomeThing Went Wrong :( ,\n";
+}
+
+sleep(3);
+int sh = fr+ar+en+ss+pc+rn+ma+sc;
+if (sh > 320){
+    cout << "  [+] U re Percentage Wrong,Brah !!\n\n\n";
+}else if(sh == 320){
+    cout << "  [+] U re Working In Tel Aviv,Brah :).\n\n\n";
+}
+else{
+    int p =sh*100/320 ; // certificate 
+    cout << "\n\n\n    =========\n";
+    cout << "    [+] Ur Certificate Precent : "<<sh<<" %.\n";
+    cout << "    =========\n\n\n\n";
+
+}
+sleep(5);
+cout << "    [+] Good Bye "<<li<<" :)\n\n\n"<<"    [+] This Script Is Written By Salem For Eng. Hassan Exam :)\n\n\n\n";
+    return 0;
+}
